271b2acaf4dda438fe9deae4aba46b87b3dfc59d
Jackie098 <carlosmiranda19122@gmail.com>
2020-04-02 03:11:01 -0300

    created pages and routes for: 'Main' and 'Repository'

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: 'babel-eslin',
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
