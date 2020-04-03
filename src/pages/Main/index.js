b1f807a89ac7ee287046db162597ef8d3f907307
Jackie098 <carlosmiranda19122@gmail.com>
2020-04-03 19:40:14 -0300

    stylizing 'Main' page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  // 'error' é propriedade de 'Title' e seu valor é 'true'
-  return <Title>Hello world</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
