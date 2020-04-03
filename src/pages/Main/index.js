5e6ea0ab6448ef81bac1f6c86e61960b799ec793
Jackie098 <carlosmiranda19122@gmail.com>
2020-04-03 19:09:10 -0300

    created folder with global css file

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,11 +4,7 @@ import { Title } from './styles';
 
 function Main() {
   // 'error' é propriedade de 'Title' e seu valor é 'true'
-  return (
-    <Title error>
-      Main<small>menor</small>
-    </Title>
-  );
+  return <Title>Hello world</Title>;
 }
 
 export default Main;
